660407c274bc8bb43f92cf86ff3c3acfd86ec296
Jarvis Schultz <jschultz@u.northwestern.edu>
2012-01-17 18:31:14 -0600

    found out I had to recompile this due to some ROS updates

--- msg_gen/lisp/_package.lisp
+++ msg_gen/lisp/_package.lisp
@@ -1,13 +1,13 @@
 (cl:defpackage puppeteer_msgs-msg
   (:use )
   (:export
-   "<ROBOTPOSE>"
-   "ROBOTPOSE"
-   "<STATE>"
-   "STATE"
-   "<POINTPLUS>"
-   "POINTPLUS"
    "<ROBOTCOMMANDS>"
    "ROBOTCOMMANDS"
+   "<ROBOTPOSE>"
+   "ROBOTPOSE"
+   "<POINTPLUS>"
+   "POINTPLUS"
+   "<STATE>"
+   "STATE"
   ))
 
